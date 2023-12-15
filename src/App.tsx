7e5e7dc5866b37914969365faf0cb688b0438ee4
Logan Matheny <loganpaulmatheny@gmail.com>
2023-12-15 13:58:06 -0500

    Create basic components and get request

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
+// Components 
+import Item from "./Item/Item"
+import  Drawer  from "@mui/material/Drawer";
+import LinearProgress from "@mui/material/LinearProgress";
+import Grid from "@mui/material/Grid";
+import AddShoppingCart  from "@mui/icons-material";
+import Badge from "@mui/material/Badge";
+// Styles 
+import {Wrapper} from './App.styles'
+// Types 
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+// the TS part of this dictates that it is an async function that should return a Promise of an array of CartItemTypes
+const getProducts = async (): Promise<CartItemType[]> => {
+  // first await - convert to json 
+  // inside await - api call 
+  return await (await fetch ('https://fakestoreapi.com/products' )).json();
+  
+}
 
 function App() {
+  // useQuery is a hook and the destructured variables hold the results from the hook
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts)
+  console.log(data)
+  
+  const getTotalItems = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleRemoveFromCart = () => null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {isLoading && <LinearProgress />}
+    {error && <div>Something went wrong</div>}
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4} >
+            <Item item={item} handleAddToCart={handleAddToCart}/>
+          </Grid>
+        ))}
+
+      </Grid>
+    </Wrapper>
+  </>
   );
 }
 
---
+++ src/Item/Item.styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+// This is where your css styling would / will go
+
+`;
